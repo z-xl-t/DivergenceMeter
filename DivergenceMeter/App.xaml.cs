2fc082270c0edee22a69c8e4e297536717637aee
sanshiliuxiao <sanshiliuxiao@foxmail.com>
2020-05-30 21:36:57 +0800

    图片替换数字（时钟显示） 异性窗体（无标题栏） 异性窗体下拖动 任务栏图标隐藏 系统小托盘（Hardcodet.NotifyIcon.Wpf）

--- DivergenceMeter/App.xaml.cs
+++ DivergenceMeter/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
+using Hardcodet.Wpf.TaskbarNotification;
 
 namespace DivergenceMeter
 {
@@ -13,5 +15,18 @@ namespace DivergenceMeter
     /// </summary>
     public partial class App : Application
     {
+        // 覆写 OnStartup 函数
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var taskbar = (TaskbarIcon) FindResource("Taskbar");
+            base.OnStartup(e);
+        }
+        // 退出应用
+        // 配合 app.xaml 中 ShutdownMode 属性使用
+        private void ClickToExit(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
+   
 }
--- DivergenceMeter/MainWindow.xaml.cs
+++ DivergenceMeter/MainWindow.xaml.cs
@@ -13,7 +13,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
-
 namespace DivergenceMeter
 {
     /// <summary>
@@ -22,14 +21,29 @@ namespace DivergenceMeter
     
     public partial class MainWindow : Window
     {
+        private int WindowsInitWidth = 1440;
+        private int windowsInitHeight = 460;
+        private double scale = 0.2;
         private DateTime _nowTime;
         private DispatcherTimer _meterClock;
+        private List<BitmapImage> AllImages = new List<BitmapImage>();
+        #region 主函数
         public MainWindow()
         {
             InitializeComponent();
+            this.Width = WindowsInitWidth * scale;
+            this.Height = windowsInitHeight * scale;
+            LoadAllImage(ref AllImages);
             StartClock();
         }
-
+        #endregion
+        #region 窗体拖动事件
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+        #endregion
+        #region 开启计时器
         private void StartClock()
         {
             if (_meterClock == null)
@@ -41,7 +55,7 @@ namespace DivergenceMeter
             }
 
         }
-
+        #endregion
         #region 计时器事件函数
         private  void Clock_Tick(object sender, EventArgs e)
         {
@@ -50,27 +64,45 @@ namespace DivergenceMeter
             var minute = _nowTime.Minute;
             var second = _nowTime.Second;
             var millisecond = _nowTime.Millisecond;
-            var aa = hour < 10 ? "0" : Convert.ToString(hour / 10);
-            var bb = hour < 10 ? Convert.ToString(hour) : Convert.ToString(hour % 10);
-            var cc = millisecond < 500 ? "left" : "right";
-            var dd = minute < 10 ? "0" : Convert.ToString(minute / 10);
-            var ee = minute < 10 ? Convert.ToString(minute) : Convert.ToString(minute % 10);
-            var ff = millisecond < 500 ? "left" : "right";
-            var gg = second < 10 ? "0" : Convert.ToString(second / 10);
-            var hh = second < 10 ? Convert.ToString(second) : Convert.ToString(second % 10);
-            
-            A.Text = Convert.ToString(aa);
-            B.Text = Convert.ToString(bb);
-            C.Text = Convert.ToString(cc);
-            D.Text = Convert.ToString(dd);
-            E.Text = Convert.ToString(ee);
-            F.Text = Convert.ToString(ff);
-            G.Text = Convert.ToString(gg);
-            H.Text = Convert.ToString(hh);
+            var aa = hour < 10 ? 0 : hour / 10;
+            var bb = hour < 10 ? hour : hour % 10;
+            var cc = millisecond < 500 ? 11 : 12;
+            var dd = minute < 10 ? 0 : minute / 10;
+            var ee = minute < 10 ? minute : minute % 10;
+            var ff = millisecond < 500 ? 11 : 12;
+            var gg = second < 10 ? 0 :second / 10;
+            var hh = second < 10 ? second : second % 10;
+
+            A.Source = AllImages[aa];
+            B.Source = AllImages[bb];
+            C.Source = AllImages[cc];
+            D.Source = AllImages[dd];
+            E.Source = AllImages[ee];
+            F.Source = AllImages[ff];
+            G.Source = AllImages[gg];
+            H.Source = AllImages[hh];
         }
 
         #endregion
-       
+        #region 加载所有图片
+        private void LoadAllImage( ref List<BitmapImage> images)
+        {
+            int[] num = new int[] {0, 1, 2, 3, 4, 5,6,7,8,9,10,11,12};
+
+            foreach (var item in num)
+            {
+                String imagePath = @$"/images/{item}.png";
+                BitmapImage bi = new BitmapImage();
+                bi.BeginInit();
+                bi.UriSource = new Uri(imagePath,UriKind.RelativeOrAbsolute);
+                bi.EndInit();
+                images.Add(bi); 
+            }
+
+            
+        }
+
+        #endregion
         
     }
 }
